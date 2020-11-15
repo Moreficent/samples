8d39ceb1fd3a84d146a26056100bc4b5ed5f44a7
Harshad Deo <harshad@moreficent.com>
2020-11-15 17:34:12 +0530

    add spring webapp samples

---
+++ javawebapp/src/test/java/com/moreficent/samples/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreficent.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
