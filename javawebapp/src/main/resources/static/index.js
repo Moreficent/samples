1e16406985cc43469cd9dbe1e03a6dfa38eacdc5
Harshad Deo <harshad@moreficent.com>
2020-11-15 20:19:33 +0530

    add basic UI for java web app

--- javawebapp/src/main/resources/static/index.js
+++ javawebapp/src/main/resources/static/index.js
@@ -1,1 +1,42 @@
-console.log('Hmmm');
+const input = document.getElementById('quesinp')
+
+const deck = document.getElementById('deck');
+
+function buildNode(question, answer){
+    const col = document.createElement('div');
+    col.className = 'col mb-4'
+
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.className = 'card-title';
+    cardTitle.innerHTML = question;
+
+    const cardText = document.createElement('p');
+    cardText.className = 'card-text';
+    cardText.innerHTML = answer;
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    return col;
+}
+
+const btn = document.getElementById('quesbtn')
+btn.onclick = () => {
+    const question = input.value;
+    if (!question){
+        console.warn('No Question');
+        return;
+    }
+    input.value = ''
+    const node = buildNode(question, 'yes');
+    deck.insertBefore(node, deck.firstChild);
+}
+
+console.log('Ho gaya');
